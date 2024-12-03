97ad473a9930cfe9685de34ccf83a365f0617422
Xer4n <simenandersen98@gmail.com>
2024-12-03 16:25:37 +0100

    Day 3 part 2 solved

---
+++ day3/day3part2.py
@@ -0,0 +1,30 @@
+import re
+total = 0
+
+file = "input.txt"
+
+def open_file(file):
+    with open(file, "r") as f:
+        line = f.read().strip()
+
+    return line
+
+line = open_file(file)
+
+
+match = re.findall(r"(?:mul\((\d+),(\d+)\))|(do\(\)|don't\(\))", line)
+print(match)
+
+do = True
+for i in match:
+    if i[2] == "" and do:
+        total += int(i[0]) * int(i[1])
+    else:
+        print(i)
+        if i[2] == "do()":
+            do = True
+        else:
+            do = False
+
+
+print(total)
